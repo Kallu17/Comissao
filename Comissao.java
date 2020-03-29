ba92cff06c8238d39c8229a88722e76bb4802d44
Kallu17 <56933682+Kallu17@users.noreply.github.com>
2020-03-29 16:21:28 -0300

    Create Comissao.java

---
+++ Comissao.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.text.DecimalFormat;
+import java.math.RoundingMode;
+
+public class Comissão{
+    public static void main(String[] args){
+        DecimalFormat real = new DecimalFormat("R$#.00");
+        int comissao = 10;
+        
+        System.out.println("Gastos cliente: ");
+        double gastos = new Scanner(System.in).nextDouble();
+        
+        double taxaServico = gastos+((comissao*gastos)/100);
+        System.out.println("A taxa de serviço é de "+real.format(taxaServico));
+    }
+}
